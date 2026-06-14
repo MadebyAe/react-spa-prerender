5c4c50acc531517c8df8c60d36c5f293df5510b4
Ae <hi@angel-estrada.com>
2026-06-14 14:28:41 -0700

    feat(prerender): parallelize route generation with concurrency option

--- utils/normalizeRspOptions.js
+++ utils/normalizeRspOptions.js
@@ -13,6 +13,7 @@ module.exports = function (options) {
     routes: options.routes || [],
     port: options.port || 3000,
     buildDirectory: options.buildDirectory || './build',
+    concurrency: options.concurrency || 4,
     engine
   };
 };
